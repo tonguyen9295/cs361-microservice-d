f7a8809242a867bcd09c3fdcc7601c1885096ad1
Tony Nguyen <91217918+tonguyen9295@users.noreply.github.com>
2024-05-11 16:38:42 -0500

    backend email validation
    backend email validation set up with using https://proofy.io/ that returns 1 to frontend if email is valid, or 0 to frontend if email is not valid
    need to develop frontend in react to pass email to backend for verification

---
+++ app.js
@@ -0,0 +1,45 @@
+// run microservice D at port 3004
+const PORT = 3004; 
+
+// require express and cors, initialize express and name it app
+const express = require('express');   
+const cors = require('cors') 
+const app = express();      
+
+// instance of express app will be used to make request for data from MySQL database and process json objects
+app.use(express.json())
+app.use(cors())
+
+// api credential for email validation
+const userId = 61565;
+const apiKey = "rzoz8geNlCIbYySrRnKh"
+
+// check if email is valid
+function checkValidEmail(value) {
+    const valid = 1;
+    if (value === valid) {
+        return {emailValid : 1}
+    } return {emailValid : 0}
+}
+
+// get status of email validity
+app.get("/contact/:email", async (req, res) => {
+    try {
+        const { email } = req.params;
+        const response = await fetch(`https://api.proofy.io/verifyaddr?aid=${userId}&key=${apiKey}&email=${email}`);
+        const cid = await response.json();
+        const numOfCid = cid["cid"];
+        const resultResponse = await fetch(`https://api.proofy.io/getresult?aid=${userId}&key=${apiKey}&cid=${numOfCid}`);
+        const checkResults = await resultResponse.json();
+        const outcome = checkResults.result[0].status;
+        res.json(checkValidEmail(outcome));
+    } catch (error) {
+        console.error('Error getting email validation data:', error);
+        res.status(500).json({ error: 'Failed to get email validation data' });
+    }
+});
+
+// turn on microservice D
+app.listen(PORT, () => { 
+    console.log(`Microservice A is live at port ${PORT}`);
+});
